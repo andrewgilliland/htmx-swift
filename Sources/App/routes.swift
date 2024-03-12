9d069befb18b98c4eacc8720cd6ceb3971386a08
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-03-12 10:08:11 -0500

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,18 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-app.get { req async in
+    let file = FileMiddleware(publicDirectory: app.directory.publicDirectory)
+    app.middleware.use(file)
+    
+    app.get { req async in
         "It works!"
     }
 
     app.get("hello") { req async -> String in
         "Hello, world!"
+    }
+    
+    app.get("users") { req async -> String in
+        "Biff Tannen"
     }
 }
